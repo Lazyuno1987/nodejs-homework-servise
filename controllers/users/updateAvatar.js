35c5ec1ef86862be151f3ac06fbda9cd68cad3c7
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-10-31 19:54:33 +0300

    end

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,36 +1,35 @@
-const { User } = require('../../models/user')
+const { User } = require("../../models/user");
 
-const path = require('path');
-const fs = require('fs').promises;
+const path = require("path");
+const fs = require("fs").promises;
 
-const avatarDir = path.join(__dirname, "../../", "public", 'avatars')
+const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 
-const Jimp  =  require('jimp') ;
-
+const Jimp = require("jimp");
 
 const updateAvatar = async (req, res) => {
-    const { path: tempUpload, originalname } = req.file;
-    const { _id: id } = req.user;
-    const imageName = `${id}_${originalname}`
-       
-    try {
-        const resultUpload = path.join(avatarDir, imageName)
-        Jimp.read(tempUpload, (err, image)  =>  { 
-           if (err) {
-      throw  err 
-           } 
-            image.resize(250, 250).write(resultUpload) 
-            }) ;
-        await fs.rename(tempUpload, resultUpload)
-        const avatar = path.join("public", "avatars", imageName)
-    await User.findByIdAndUpdate(req.user._id, { avatar })
-        res.json({
-            avatar
-        })
-    } catch (error) {
-        await fs.unlink(tempUpload)
-        throw error
-    }
-}
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const imageName = `${id}_${originalname}`;
+
+  try {
+    const resultUpload = path.join(avatarDir, imageName);
+    Jimp.read(tempUpload, (err, image) => {
+      if (err) {
+        throw err;
+      }
+      image.resize(250, 250).write(resultUpload);
+    });
+    await fs.rename(tempUpload, resultUpload);
+    const avatar = path.join("public", "avatars", imageName);
+    await User.findByIdAndUpdate(req.user._id, { avatar });
+    res.json({
+      avatar,
+    });
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    throw error;
+  }
+};
 
 module.exports = updateAvatar;
